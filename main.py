a91ac481c86a890fea1a452982261f73a5b55325
Santhosh Kumar Rajamanickam <santhosh.rajamanickam@student.uva.nl>
2018-04-25 09:09:30 +0200

    Add loglikehood for convergence IBM2

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ english_training_filepath = "./training/hansards.36.2.e"
 french_training_filepath = "./training/hansards.36.2.f"
 
 corpus = ParallelCorpus(english_training_filepath, french_training_filepath)
-t, q = EM_algorithm(corpus.english_sentences[:100], corpus.french_sentences[:100])
+t, q = EM_algorithm(corpus.english_sentences[:1000], corpus.french_sentences[:1000])
