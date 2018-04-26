ac94b0c182a7b85a2e35c492e1dde0d738b9e32d
Simone van Bruggen <simonevanbruggen@hotmail.com>
2018-04-26 14:23:35 +0200

    Copied functions, added map_to_UNK

---
+++ data_load.py
@@ -0,0 +1,57 @@
+# import pandas as pd
+from collections import Counter
+
+class ParallelCorpus:
+
+    def __init__(self, english_training_path, french_training_path, english_testing_path, french_testing_path):
+
+        self.training_english = []
+        self.training_french = []
+        self.testing_english = []
+        self.testing_french = []
+
+        print("============")
+        print("Loading Data")
+        print("============")
+
+        with open(english_training_path, encoding='utf8') as file:
+            for i, line in enumerate(file):
+                sentence = [x.lower() for x in line.split()] # convert all words to lowercase
+                sentence.insert(0,'NULL') # adding a NULL word
+                self.training_english.append(sentence)
+            #self.training_english = self.map_to_unk(20,self.training_english)
+
+        with open(french_training_path, encoding='utf8') as file:
+            for i, line in enumerate(file):
+                sentence = [x.lower() for x in line.split()] # convert all words to lowercase
+                self.training_french.append(sentence)
+            #self.training_french = self.map_to_unk(20,self.training_french)
+
+        with open(english_testing_path, encoding='utf8') as file:
+            for i, line in enumerate(file):
+                sentence = [x.lower() for x in line.split()] # convert all words to lowercase
+                sentence.insert(0,'NULL') # adding a NULL word
+                self.testing_english.append(sentence)
+
+        with open(french_testing_path, encoding='utf8') as file:
+            for i, line in enumerate(file):
+                sentence = [x.lower() for x in line.split()] # convert all words to lowercase
+                self.testing_french.append(sentence)
+
+
+        print("Number of English sentences in the training set: {}".format(len(self.training_english)))
+        print("Number of French sentences in the training set: {}".format(len(self.training_french)))
+        print("Number of English sentences in the testing set: {}".format(len(self.testing_english)))
+        print("Number of French sentences in the testing set: {}".format(len(self.testing_french)))
+
+
+    def map_to_unk(self, k, training):
+        counts = Counter(w for sent in training for w in sent)
+        counted_once = [w for w, count in counts.items() if count == 1]
+        counted_once = counted_once[0:k]
+        for i, sentence in enumerate(training):
+            for j, word in enumerate(sentence):
+                if word in counted_once:
+                    sentence[j] = 'UNK'
+            training[i] = sentence
+        return training
---
+++ main.py
@@ -0,0 +1,27 @@
+from data_load import ParallelCorpus
+from IBM import IBM
+
+english_training_filepath = "./training/hansards.36.2.e"
+french_training_filepath = "./training/hansards.36.2.f"
+english_testing_filepath = "./validation/dev.e"
+french_testing_filepath = "./validation/dev.f"
+gold_standard_filepath = "./validation/dev.wa.nonullalign"
+model = 1
+num_iteration = 10
+
+corpus = ParallelCorpus(english_training_filepath, french_training_filepath, \
+                        english_testing_filepath, french_testing_filepath)
+
+IBM_model_1 = IBM(model, corpus.training_english[:1000], corpus.training_french[:1000])
+IBM_model_1.load_test_sentences(corpus.testing_english, corpus.testing_french)
+IBM_model_1.em_algorithm(num_iteration)
+test_alignments = IBM_model_1.viterbi_alignment()
+IBM_model_1.calculate_aer(gold_standard_filepath, test_alignments)
+
+# IBM_model_2 = IBM(model, corpus.training_english, corpus.training_french)
+# IBM_model_2.load_test_sentences(corpus.testing_english, corpus.testing_french)
+# IBM_model_2.em_algorithm(num_iteration)
+# test_alignments = IBM_model_2.viterbi_alignment()
+# IBM_model_2.calculate_aer(gold_standard_filepath, test_alignments)
+
+# t, q = EM_algorithm(corpus.training_english[:1000], corpus.training_french[:1000])
