9220c8552a24b48aa8de03c800c000d4ef4c8772
Simone van Bruggen <simonevanbruggen@hotmail.com>
2018-05-24 17:11:09 +0200

    Move data processing to separate function

--- Project 2/data_process.py
+++ Project 2/data_process.py
@@ -29,68 +29,70 @@ def revert_BPE(sentence):
 # pip install subword-nmt
 # pip install https://github.com/rsennrich/subword-nmt/archive/master.zip
 
-print("Starting Preprocessing....")
 
-# Pre-processing English corpora
-print("Processing English language corpora....")
-print("Processing training set....")
-print("Processing validation set....")
-print("Processing testing set....")
-# Tokenize
-os.system("perl tokenizer.perl -l en < ./data/val/val.en > ./data/val/val_tok.en")
-os.system("perl tokenizer.perl -l en < ./data/train/train.en > ./data/train/train_tok.en")
-os.system("perl tokenizer.perl -l en < ./data/test/test.en > ./data/test/test_tok.en")
-print("Done Tokenizing!!")
-# Lowercasing
-os.system("perl lowercase.perl -l en < ./data/val/val_tok.en > ./data/val/val_lower.en")
-os.system("perl lowercase.perl -l en < ./data/train/train_tok.en > ./data/train/train_lower.en")
-os.system("perl lowercase.perl -l en < ./data/test/test_tok.en > ./data/test/test_lower.en")
-print("Done Lowercasing!!")
-# BPE
-os.system(
-    "subword-nmt learn-bpe -s {} < ./data/val/val_lower.en > ./data/val/val_code.en.bpe".format(num_merge_operation))
-os.system("subword-nmt apply-bpe -c ./data/val/val_code.en.bpe < ./data/val/val_lower.en > ./data/val/val_final.en")
-os.system(
-    "subword-nmt learn-bpe -s {} < ./data/train/train_lower.en > ./data/train/train_code.en.bpe".format(
-        num_merge_operation))
-os.system(
-    "subword-nmt apply-bpe -c ./data/train/train_code.en.bpe < ./data/train/train_lower.en > ./data/train/train_final.en")
-os.system(
-    "subword-nmt learn-bpe -s {} < ./data/test/test_lower.en > ./data/test/test_code.en.bpe".format(
-        num_merge_operation))
-os.system(
-    "subword-nmt apply-bpe -c ./data/test/test_code.en.bpe < ./data/test/test_lower.en > ./data/test/test_final.en")
-print("Done BPE!!")
+def preprocess():
+    print("Starting Preprocessing....")
 
-# Pre-processing French corpora
-print("Processing French language corpora....")
-print("Processing training set....")
-print("Processing validation set....")
-print("Processing testing set....")
-# Tokenize
-os.system("perl tokenizer.perl -l fr < ./data/val/val.fr > ./data/val/val_tok.fr")
-os.system("perl tokenizer.perl -l fr < ./data/train/train.fr > ./data/train/train_tok.fr")
-os.system("perl tokenizer.perl -l fr < ./data/test/test.fr > ./data/test/test_tok.fr")
-print("Done Tokenizing!!")
-# Lowercasing
-os.system("perl lowercase.perl -l fr < ./data/val/val_tok.fr > ./data/val/val_lower.fr")
-os.system("perl lowercase.perl -l fr < ./data/train/train_tok.fr > ./data/train/train_lower.fr")
-os.system("perl lowercase.perl -l fr < ./data/test/test_tok.fr > ./data/test/test_lower.fr")
-print("Done Lowercasing!!")
-# BPE
-os.system(
-    "subword-nmt learn-bpe -s {} < ./data/val/val_lower.fr > ./data/val/val_code.fr.bpe".format(num_merge_operation))
-os.system("subword-nmt apply-bpe -c ./data/val/val_code.fr.bpe < ./data/val/val_lower.fr > ./data/val/val_final.fr")
-os.system(
-    "subword-nmt learn-bpe -s {} < ./data/train/train_lower.fr > ./data/train/train_code.fr.bpe".format(
-        num_merge_operation))
-os.system(
-    "subword-nmt apply-bpe -c ./data/train/train_code.fr.bpe < ./data/train/train_lower.fr > ./data/train/train_final.fr")
-os.system(
-    "subword-nmt learn-bpe -s {} < ./data/test/test_lower.fr > ./data/test/test_code.fr.bpe".format(
-        num_merge_operation))
-os.system(
-    "subword-nmt apply-bpe -c ./data/test/test_code.fr.bpe < ./data/test/test_lower.fr > ./data/test/test_final.fr")
-print("Done BPE!!")
+    # Pre-processing English corpora
+    print("Processing English language corpora....")
+    print("Processing training set....")
+    print("Processing validation set....")
+    print("Processing testing set....")
+    # Tokenize
+    os.system("perl tokenizer.perl -l en < ./data/val/val.en > ./data/val/val_tok.en")
+    os.system("perl tokenizer.perl -l en < ./data/train/train.en > ./data/train/train_tok.en")
+    os.system("perl tokenizer.perl -l en < ./data/test/test.en > ./data/test/test_tok.en")
+    print("Done Tokenizing!!")
+    # Lowercasing
+    os.system("perl lowercase.perl -l en < ./data/val/val_tok.en > ./data/val/val_lower.en")
+    os.system("perl lowercase.perl -l en < ./data/train/train_tok.en > ./data/train/train_lower.en")
+    os.system("perl lowercase.perl -l en < ./data/test/test_tok.en > ./data/test/test_lower.en")
+    print("Done Lowercasing!!")
+    # BPE
+    os.system(
+        "subword-nmt learn-bpe -s {} < ./data/val/val_lower.en > ./data/val/val_code.en.bpe".format(num_merge_operation))
+    os.system("subword-nmt apply-bpe -c ./data/val/val_code.en.bpe < ./data/val/val_lower.en > ./data/val/val_final.en")
+    os.system(
+        "subword-nmt learn-bpe -s {} < ./data/train/train_lower.en > ./data/train/train_code.en.bpe".format(
+            num_merge_operation))
+    os.system(
+        "subword-nmt apply-bpe -c ./data/train/train_code.en.bpe < ./data/train/train_lower.en > ./data/train/train_final.en")
+    os.system(
+        "subword-nmt learn-bpe -s {} < ./data/test/test_lower.en > ./data/test/test_code.en.bpe".format(
+            num_merge_operation))
+    os.system(
+        "subword-nmt apply-bpe -c ./data/test/test_code.en.bpe < ./data/test/test_lower.en > ./data/test/test_final.en")
+    print("Done BPE!!")
 
-print("Done Preprocessing!!!")
+    # Pre-processing French corpora
+    print("Processing French language corpora....")
+    print("Processing training set....")
+    print("Processing validation set....")
+    print("Processing testing set....")
+    # Tokenize
+    os.system("perl tokenizer.perl -l fr < ./data/val/val.fr > ./data/val/val_tok.fr")
+    os.system("perl tokenizer.perl -l fr < ./data/train/train.fr > ./data/train/train_tok.fr")
+    os.system("perl tokenizer.perl -l fr < ./data/test/test.fr > ./data/test/test_tok.fr")
+    print("Done Tokenizing!!")
+    # Lowercasing
+    os.system("perl lowercase.perl -l fr < ./data/val/val_tok.fr > ./data/val/val_lower.fr")
+    os.system("perl lowercase.perl -l fr < ./data/train/train_tok.fr > ./data/train/train_lower.fr")
+    os.system("perl lowercase.perl -l fr < ./data/test/test_tok.fr > ./data/test/test_lower.fr")
+    print("Done Lowercasing!!")
+    # BPE
+    os.system(
+        "subword-nmt learn-bpe -s {} < ./data/val/val_lower.fr > ./data/val/val_code.fr.bpe".format(num_merge_operation))
+    os.system("subword-nmt apply-bpe -c ./data/val/val_code.fr.bpe < ./data/val/val_lower.fr > ./data/val/val_final.fr")
+    os.system(
+        "subword-nmt learn-bpe -s {} < ./data/train/train_lower.fr > ./data/train/train_code.fr.bpe".format(
+            num_merge_operation))
+    os.system(
+        "subword-nmt apply-bpe -c ./data/train/train_code.fr.bpe < ./data/train/train_lower.fr > ./data/train/train_final.fr")
+    os.system(
+        "subword-nmt learn-bpe -s {} < ./data/test/test_lower.fr > ./data/test/test_code.fr.bpe".format(
+            num_merge_operation))
+    os.system(
+        "subword-nmt apply-bpe -c ./data/test/test_code.fr.bpe < ./data/test/test_lower.fr > ./data/test/test_final.fr")
+    print("Done BPE!!")
+
+    print("Done Preprocessing!!!")
