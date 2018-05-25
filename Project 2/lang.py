a05b1da8a9ddfb69e902763d9e3302bb700f1cf1
Santhosh Kumar Rajamanickam <santhosh.rajamanickam@student.uva.nl>
2018-05-26 00:47:26 +0200

    Update lang.py

--- Project 2/lang.py
+++ Project 2/lang.py
@@ -1,16 +1,23 @@
+PAD_token = 0
+SOS_token = 1
+EOS_token = 2
+
+
 class Lang:
+
     def __init__(self, name):
         self.name = name
+        self.trimmed = False
         self.word2index = {}
         self.word2count = {}
-        self.index2word = {0: "SOS", 1: "EOS", -1: "-UNK-"}
-        self.n_words = 2  # Count SOS and EOS
+        self.index2word = {0: "PAD", 1: "SOS", 2: "EOS", -1: "-UNK-"}
+        self.n_words = 3  # Count default tokens
 
-    def addSentence(self, sentence):
+    def add_sentence(self, sentence):
         for word in sentence:
             self.addWord(word)
 
-    def addWord(self, word):
+    def add_word(self, word):
         if word not in self.word2index:
             self.word2index[word] = self.n_words
             self.word2count[word] = 1
@@ -18,3 +25,29 @@ class Lang:
             self.n_words += 1
         else:
             self.word2count[word] += 1
+
+    # Remove words below a certain count threshold
+    def trim(self, min_count):
+        if self.trimmed: return
+        self.trimmed = True
+
+        keep_words = []
+
+        for k, v in self.word2count.items():
+            if v >= min_count:
+                keep_words.append(k)
+
+        print('keep_words %s / %s = %.4f' % (
+            len(keep_words), len(self.word2index), len(keep_words) / len(self.word2index)
+        ))
+
+        # Reinitialize dictionaries
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {0: "PAD", 1: "SOS", 2: "EOS", -1: "-UNK-"}
+        self.n_words = 3  # Count default tokens
+
+        for word in keep_words:
+            self.index_word(word)
+
+
